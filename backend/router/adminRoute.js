fb48f852a2ac03a584191cd363855b399a820f71
ShyamKumarA <shyamkumarbeypore@gmail.com>
2024-02-22 16:00:56 +0530

    dropdown locations added

--- backend/router/adminRoute.js
+++ backend/router/adminRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 const adminRouter = express.Router();
-import {addDistrict, addPanchayath, addState, addZonal, adminLogin,  forgotPassword, viewAllDistricts, viewAllPanchayaths, viewAllZonals, viewParamsDistricts, viewStates} from "../controller/adminController.js";
+import {addDistrict, addPanchayath, addState, addZonal, adminLogin,  forgotPassword, viewAllDistricts, viewAllPanchayaths, viewAllZonals, viewParamsDistricts, viewParamsPanchayaths, viewParamsZonals, viewStates} from "../controller/adminController.js";
 import { protectAdmin } from "../middleware/authMiddleware.js";
 
 adminRouter.post("/admin-login", adminLogin);
@@ -16,7 +16,9 @@ adminRouter.get("/view-states", protectAdmin,viewStates);
 adminRouter.get("/view-all-districts", protectAdmin,viewAllDistricts);
 adminRouter.get("/view-all-zonals", protectAdmin,viewAllZonals);
 adminRouter.get("/view-all-panchayaths", protectAdmin,viewAllPanchayaths);
-adminRouter.get("/view-params-districts/:id", protectAdmin,viewParamsDistricts);
+adminRouter.get("/view-dropdown-districts/:id", protectAdmin,viewParamsDistricts);
+adminRouter.get("/view-dropdown-zonals/:id", protectAdmin,viewParamsZonals);
+adminRouter.get("/view-dropdown-panchayaths/:id", protectAdmin,viewParamsPanchayaths);
 
 
 

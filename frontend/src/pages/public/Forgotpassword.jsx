43db6d2aac575c4b63dc49a8d551454588e0e667
VysakhSdev <vysakhs807@gmail.com>
2024-03-13 13:16:27 +0530

    file name toast to toastify changed

--- frontend/src/pages/public/Forgotpassword.jsx
+++ frontend/src/pages/public/Forgotpassword.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import { Show_Toast } from "../../utils/Toast";
+import { Show_Toast } from "../../utils/Toastify";
 import { Button, Form } from "react-bootstrap";
 import { ContextData } from "../../Services/Context";
 import { update_PasswordURL } from "../../utils/Constants";

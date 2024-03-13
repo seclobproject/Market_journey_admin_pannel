d5b128ec6cb0b49b03fefd1a47d8d9f2bc14fe86
VysakhSdev <vysakhs807@gmail.com>
2024-03-13 15:18:10 +0530

    base change

--- frontend/src/Services/Base_url.jsx
+++ frontend/src/Services/Base_url.jsx
@@ -1,3 +1,3 @@
-// export const Base_url = "https://marketjourney.in";
+export const Base_url = "https://marketjourney.in";
 
-export const Base_url="http://192.168.29.152:6003"
+// export const Base_url="http://192.168.29.152:6003"s

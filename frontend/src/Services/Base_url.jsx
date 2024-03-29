48763131154d99df7ddfba8961996429d2f9fbeb
ShyamKumarA <shyamkumarbeypore@gmail.com>
2024-03-29 16:40:20 +0530

    base url changed

--- frontend/src/Services/Base_url.jsx
+++ frontend/src/Services/Base_url.jsx
@@ -1,5 +1,5 @@
 // export const Base_url = "https://marketjourney.in";//main
-//  export const Base_url="http://192.168.29.152:6003"
+ export const Base_url="http://192.168.29.152:6003"
 // export const Base_url="http://192.168.29.77:6003"//localhost:6003
 //export const Base_url="https://seclobtest.site"//test server
-export const Base_url="http://localhost:6003"
+// export const Base_url="http://localhost:6003"

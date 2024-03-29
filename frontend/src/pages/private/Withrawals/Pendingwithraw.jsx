264dee27d13ca6ad558a76bb37b5aea92f505851
VysakhSdev <vysakhs807@gmail.com>
2024-03-29 16:36:39 +0530

    withrawals fixed

--- frontend/src/pages/private/Withrawals/Pendingwithraw.jsx
+++ frontend/src/pages/private/Withrawals/Pendingwithraw.jsx
@@ -1,9 +1,81 @@
-import React, { useState } from 'react'
-import { SlideMotion } from '../../../libs/FramerMotion'
-import Loader from '../../../Components/Loader';
+import React, { useEffect, useState } from "react";
+import { SlideMotion } from "../../../libs/FramerMotion";
+import Loader from "../../../Components/Loader";
+import { ApiCall } from "../../../Services/Api";
+import { acceptrequestUrl, viewwithrawalrequesteUrl } from "../../../utils/Constants";
+import moment from "moment";
+import ModalComponent from "../../../Components/ModalComponet";
+import { Pagination, Stack } from "@mui/material";
+import { Show_Toast } from "../../../utils/Toastify";
+import { useNavigate } from "react-router-dom";
 
 function Pendingwithraw() {
-    const [isLoading, setIsLoading] = useState(false);
+  const navigate=useNavigate()
+  const [isLoading, setIsLoading] = useState(false);
+  const [viewRequest, setViewRequest] = useState([]);
+  const [approveModal, setApproveModal] = useState({ show: false, id: null });
+  const [totalPages, setTotalPages] = useState(1);
+  const [params, setParams] = useState({
+    page: 1,
+    pageSize: 10,
+  });
+
+//----------------View Request--------------
+  const getRequest = async () => {
+
+    try {
+      setIsLoading(true);
+      const response = await ApiCall(
+        "get",
+        viewwithrawalrequesteUrl,
+        {},
+        params
+      );
+      console.log(response, "response...");
+      if (response?.status === 200) {
+        setViewRequest(response?.data?.userData);
+        setTotalPages(response?.data?.pagination?.totalPages);
+
+        setIsLoading(false);
+      } else {
+        console.error(
+          "Error fetching request data. Unexpected status:",
+          response.status
+        );
+      }
+    } catch (error) {
+      console.error("Error fetching user request:", error);
+    }
+  };
+  //------------approve request--------------
+  const approveRequest = async () => {
+    try {
+      const action=approveModal?.action
+
+      const resposne = await ApiCall(
+        "post",
+        `${acceptrequestUrl}/${approveModal.id}`,{action}
+      );
+      console.log(resposne,"res ress res")
+      if (resposne?.status === 200) {
+        Show_Toast(resposne?.data?.msg, true);
+        setApproveModal(false);
+        getRequest();
+      }
+    } catch (error) {
+      Show_Toast(error, false);
+    }
+  };
+
+  const handlePageChange = (event, newPage) => {
+    setParams((prevParams) => ({
+      ...prevParams,
+      page: newPage,
+    }));
+  };
+  useEffect(() => {
+    getRequest();
+  }, [params]);
 
   return (
     <>
@@ -21,136 +93,122 @@ function Pendingwithraw() {
           {isLoading ? (
             <Loader />
           ) : (
-          <div className="card-body p-2 mb-2">
-            <div className="table-container table-responsive rounded-2 mb-4">
-              <table className="table border text-nowrap customize-table mb-0 align-middle">
-                <thead className="text-dark fs-4 table-light">
-                  <tr>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">SL.NO</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Name</h6>
-                    </th>
+            <div className="card-body p-2 mb-2">
+              <div className="table-container table-responsive rounded-2 mb-4">
+                <table className="table border text-nowrap customize-table mb-0 align-middle">
+                  <thead className="text-dark fs-4 table-light">
+                    <tr>
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">SL.NO</h6>
+                      </th>
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">Name</h6>
+                      </th>
 
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Sponser Name</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Email</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Phone</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Package Amount</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Date</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Image</h6>
-                    </th>
-
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Status</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">View Details</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Aprrove User</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Reject user</h6>
-                    </th>
-                    <th />
-                  </tr>
-                </thead>
-                {/* <tbody>
-                  {pendingMemberList?.length ? (
+                  
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">Email</h6>
+                      </th>
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">Phone</h6>
+                      </th>
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">Date</h6>
+                      </th>
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">
+                        Wallet Withdraw Amount 
+                        </h6>
+                      </th>
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">TDS Amount</h6>
+                      </th>
+                    
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">Withdraw Status</h6>
+                      </th>
+                     
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">Aprrove Request</h6>
+                      </th>
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">Reject Request</h6>
+                      </th>
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">View Details</h6>
+                      </th>
+                      <th />
+                    </tr>
+                  </thead>
+                  <tbody>
+                  {viewRequest?.length ? (
                     <>
-                      {pendingMemberList.map(
-                        (members, index) => (
-                          console.log(members, "45678"),
+                      {viewRequest.map(
+                        (request, index) => (
                           (
                             <tr key={index}>
                               <td>{index + 1}</td>
                               <td>
-                                {(members?.name &&
-                                  members.name.toUpperCase()) ||
+                                {(request?.name &&
+                                  request.name.toUpperCase()) ||
                                   "--"}
                               </td>
-                              <td>
+                              {/* <td>
                                 {(members?.sponserName &&
                                   members.sponserName.toUpperCase()) ||
                                   "--"}
-                              </td>
-                              <td>{members?.email || "--"}</td>
-                              <td>{members?.phone || "--"}</td>
-                              <td>{members?.tempPackageAmount || "0"}</td>
-                              <td>{members?.createdAt ? moment(members.createdAt).format('DD/MM/YYYY') : "--"}</td>
+                              </td> */}
+                              <td>{request?.email || "--"}</td>
+                              <td>{request?.phone || "--"}</td>
+                              <td>{request?.updatedAt ? moment(request.updatedAt).format('DD/MM/YYYY') : "--"}</td>
+                              <td>{request?.walletWithdrawAmount || "0"}</td>
+                              <td>{request?.tdsAmount || "0"}</td>
                               <td>
-                                <img
-                                  alt="images"
-                                  src={`                                  http://192.168.29.152:8000/uploads/${members?.screenshot}
-                                  `}
-                                  style={{
-                                    width: "100px",
-                                    height: "100px",
-                                    objectFit: "cover",
-                                    borderRadius: "5px",
-                                  }}
-                                />
-
-                              
-                              </td>
-                              
-                              <td>
-                                {members?.userStatus === "readyToApprove" && (
-                                  <span className="badge bg-danger rounded-3 fw-semibold">
-                                    Ready to approve
-                                  </span>
-                                )}
-                              </td>
-                              <td>
-
-<i className="fas fa-eye"    onClick={() =>   navigate('/user/details', { state: { data: members?._id } })}
-></i>   
-
-</td>
-
-                              <td>
-                                {members?.userStatus === "readyToApprove" && (
+      {request?.walletWithdrawStatus === "pending" && (
+        <span className="badge bg-danger rounded-3 fw-semibold">Pending</span>
+      )}
+    </td>
+    
+    <td>
                                   <button
                                     className="btn btn-success"
                                     onClick={() =>
                                       setApproveModal({
                                         show: true,
-                                        id: members._id,
+                                        id: request._id,
+                                        action: 'approve'
                                       })
                                     }
                                   >
                                     Approve
                                   </button>
-                                )}
                               </td>
                               <td>
-                                {members?.userStatus === "readyToApprove" && (
                                   <button
                                     className="btn btn-cancel"
                                     onClick={() =>
-                                      setrejectModal({
+                                      setApproveModal({
                                         show: true,
-                                        id: members._id,
+                                        id: request._id,
+                                        action: 'reject'
+
                                       })
                                     }
                                   >
                                     Reject
                                   </button>
-                                )}
-                              </td>
-  
+                              </td>                
+                              <td>
+
+<i className="fas fa-eye"    onClick={() =>   navigate('/user/details', { state: { data: request?._id } })}
+></i>   
+
+</td>
+                              
+                         
+        
+
+                           
                             </tr>
                           )
                         )
@@ -163,21 +221,75 @@ function Pendingwithraw() {
                       </td>
                     </tr>
                   )}
-                </tbody> */}
-              </table>
+                </tbody>
+                </table>
+              </div>
             </div>
-          </div>
-            )}
-          <div className="me-2">
-            {/* -------------------------pagination--------------------- */}
-
-            {/* -------------------------pagination--------------------- */}
+          )}
+           <div className="me-2 mb-3 d-flex ms-auto">
+            <Stack spacing={2}>
+              <Pagination
+                count={totalPages}
+                page={params.page}
+                onChange={handlePageChange}
+                color="primary"
+              />
+            </Stack>
           </div>
         </div>
       </SlideMotion>
+      <ModalComponent
+        show={approveModal.show}
+        onHide={() => {
+          setApproveModal({ show: false, id: null });
+        }}
+        centered
+        width={"500px"}
+      >
+        <div className="modal-body">
+          <div className="row mb-4">
+            <div className="col d-flex justify-content-center">
+              <i
+                style={{ fontSize: "50px", color: "#fe9423" }}
+                className="fa fa-exclamation-triangle "
+                aria-hidden="true"
+              ></i>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col d-flex justify-content-center ">
+              <h5 className="">
+              Are you sure you want to {approveModal?.action === 'approve' ? 'approve' : 'reject'} this request?              </h5>
+            </div>
+          </div>
+        </div>
 
+        <div className="modal-footer">
+          <div className="col gap-3 d-flex justify-content-center">
+            <button
+              onClick={() => {
+                setApproveModal({ show: false, id: null });
+              }}
+              type="button"
+              className="btn btn-cancel"
+              data-bs-dismiss="modal"
+            >
+              No, keep it
+            </button>
+            <button
+              type="button"
+              className="btn btn-custom text-white"
+              onClick={() => {
+                approveRequest()              
+              }}
+            >
+              Yes, {approveModal?.action === 'approve' ? 'Approve' : 'Reject'} it
+            </button>
+          </div>
+        </div>
+      </ModalComponent>
     </>
-  )
+  );
 }
 
-export default Pendingwithraw
+export default Pendingwithraw;
--- frontend/src/pages/private/Withrawals/Withrawhistroy.jsx
+++ frontend/src/pages/private/Withrawals/Withrawhistroy.jsx
@@ -1,13 +1,58 @@
-import React, { useState } from 'react'
-import Loader from '../../../Components/Loader'
-import { SlideMotion } from '../../../libs/FramerMotion';
+import React, { useEffect, useState } from "react";
+import Loader from "../../../Components/Loader";
+import { SlideMotion } from "../../../libs/FramerMotion";
+import { ApiCall } from "../../../Services/Api";
+import { viewhistoryUrl } from "../../../utils/Constants";
+import moment from "moment";
+import { useNavigate } from "react-router-dom";
+import { Pagination, Stack } from "@mui/material";
 
 function Withrawhistroy() {
-    const [isLoading, setIsLoading] = useState(false);
+  const navigate=useNavigate()
+  const [isLoading, setIsLoading] = useState(false);
+  const [params, setParams] = useState({
+    page: 1,
+    pageSize: 10,
+  });
+  const [totalPages, setTotalPages] = useState(1);
 
+  const [viewHistory, setViewHistory] = useState([]);
+
+  //-------View History-------------------
+  const getHistory = async () => {
+    try {
+      setIsLoading(true);
+      const response = await ApiCall("get", viewhistoryUrl,{},params);
+      console.log(response, "get");
+      if (response.status === 200) {
+        setViewHistory(response?.data?.allAddFundHistory);
+        setTotalPages(response?.data?.pagination?.totalPages);
+
+        setIsLoading(false);
+      } else {
+        console.error(
+          "Error fetching history data. Unexpected status:",
+          response.status
+        );
+      }
+    } catch (error) {
+      console.error("Error fetching history:", error);
+    }
+  };
+
+  const handlePageChange = (event, newPage) => {
+    setParams((prevParams) => ({
+      ...prevParams,
+      page: newPage,
+    }));
+  };
+
+  useEffect(() => {
+    getHistory();
+  }, [params]);
   return (
     <>
-  <SlideMotion>
+      <SlideMotion>
         <div className="card w-100 position-relative overflow-hidden">
           {" "}
           <div className="px-4 py-3 border-bottom d-flex align-items-center justify-content-between">
@@ -21,135 +66,98 @@ function Withrawhistroy() {
           {isLoading ? (
             <Loader />
           ) : (
-          <div className="card-body p-2 mb-2">
-            <div className="table-container table-responsive rounded-2 mb-4">
-              <table className="table border text-nowrap customize-table mb-0 align-middle">
-                <thead className="text-dark fs-4 table-light">
-                  <tr>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">SL.NO</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Name</h6>
-                    </th>
+            <div className="card-body p-2 mb-2">
+              <div className="table-container table-responsive rounded-2 mb-4">
+                <table className="table border text-nowrap customize-table mb-0 align-middle">
+                  <thead className="text-dark fs-4 table-light">
+                    <tr>
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">SL.NO</h6>
+                      </th>
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">Name</h6>
+                      </th>
 
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Sponser Name</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Email</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Phone</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Package Amount</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Date</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Image</h6>
-                    </th>
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">Franchise Type</h6>
+                      </th>
+                      
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">Date</h6>
+                      </th>
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">Requested Amount
+</h6>
+                      </th>
+                 
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">Released Amount</h6>
+                      </th>
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">
+                      Current Balance
 
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Status</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">View Details</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Aprrove User</h6>
-                    </th>
-                    <th>
-                      <h6 className="fs-4 fw-semibold mb-0">Reject user</h6>
-                    </th>
-                    <th />
-                  </tr>
-                </thead>
-                {/* <tbody>
-                  {pendingMemberList?.length ? (
+                        </h6>
+                      </th>
+                      
+                    
+
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">Status</h6>
+                      </th>
+                    
+                      <th>
+                        <h6 className="fs-4 fw-semibold mb-0">View Details</h6>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  {viewHistory?.length ? (
                     <>
-                      {pendingMemberList.map(
-                        (members, index) => (
-                          console.log(members, "45678"),
+                      {viewHistory.map(
+                        (history, index) => (
                           (
                             <tr key={index}>
                               <td>{index + 1}</td>
                               <td>
-                                {(members?.name &&
-                                  members.name.toUpperCase()) ||
+                                {(history?.name &&
+                                  history.name.toUpperCase()) ||
                                   "--"}
                               </td>
                               <td>
-                                {(members?.sponserName &&
-                                  members.sponserName.toUpperCase()) ||
+                                {(history?.franchise &&
+                                  history.franchise.toUpperCase()) ||
                                   "--"}
                               </td>
-                              <td>{members?.email || "--"}</td>
-                              <td>{members?.phone || "--"}</td>
-                              <td>{members?.tempPackageAmount || "0"}</td>
-                              <td>{members?.createdAt ? moment(members.createdAt).format('DD/MM/YYYY') : "--"}</td>
-                              <td>
-                                <img
-                                  alt="images"
-                                  src={`                                  http://192.168.29.152:8000/uploads/${members?.screenshot}
-                                  `}
-                                  style={{
-                                    width: "100px",
-                                    height: "100px",
-                                    objectFit: "cover",
-                                    borderRadius: "5px",
-                                  }}
-                                />
+                       
+           
+                              <td>{history?.createdAt ? moment(history.createdAt).format('DD/MM/YYYY') : "--"}</td>
+                              <td>{history?.requestedAmount || "0"}</td>
+                              <td>{history?.releasedAmount || "0"}</td>
+                              <td>{history?.newWalletAmount || "0"}</td>
 
-                              
-                              </td>
-                              
                               <td>
-                                {members?.userStatus === "readyToApprove" && (
-                                  <span className="badge bg-danger rounded-3 fw-semibold">
-                                    Ready to approve
-                                  </span>
-                                )}
-                              </td>
+  {history?.status === 'Approved' ? (
+    <span className="badge bg-success rounded-3 fw-semibold">
+      Approved
+    </span>
+  ) : (
+    <span className="badge bg-danger rounded-3 fw-semibold">
+      Rejected
+    </span>
+  )}
+</td>
+
+
+                 
                               <td>
 
-<i className="fas fa-eye"    onClick={() =>   navigate('/user/details', { state: { data: members?._id } })}
+<i className="fas fa-eye"    onClick={() =>   navigate('/user/details', { state: { data: history?._id } })}
 ></i>   
 
 </td>
 
-                              <td>
-                                {members?.userStatus === "readyToApprove" && (
-                                  <button
-                                    className="btn btn-success"
-                                    onClick={() =>
-                                      setApproveModal({
-                                        show: true,
-                                        id: members._id,
-                                      })
-                                    }
-                                  >
-                                    Approve
-                                  </button>
-                                )}
-                              </td>
-                              <td>
-                                {members?.userStatus === "readyToApprove" && (
-                                  <button
-                                    className="btn btn-cancel"
-                                    onClick={() =>
-                                      setrejectModal({
-                                        show: true,
-                                        id: members._id,
-                                      })
-                                    }
-                                  >
-                                    Reject
-                                  </button>
-                                )}
-                              </td>
+                             
   
                             </tr>
                           )
@@ -163,20 +171,26 @@ function Withrawhistroy() {
                       </td>
                     </tr>
                   )}
-                </tbody> */}
-              </table>
+                </tbody>
+                </table>
+              </div>
             </div>
+          )}
+          <div className="me-2 mb-3 d-flex ms-auto">
+            <Stack spacing={2}>
+              <Pagination
+                count={totalPages}
+                page={params.page}
+                onChange={handlePageChange}
+                color="primary"
+              />
+            </Stack>
           </div>
-            )}
-          <div className="me-2">
-            {/* -------------------------pagination--------------------- */}
+          </div>
 
-            {/* -------------------------pagination--------------------- */}
-          </div>
-        </div>
       </SlideMotion>
     </>
-  )
+  );
 }
 
-export default Withrawhistroy
+export default Withrawhistroy;

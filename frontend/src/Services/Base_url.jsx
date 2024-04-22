b81a0bdeee069152db770f5ef2b7f37e1880f0ec
VysakhSdev <vysakhs807@gmail.com>
2024-04-22 18:07:30 +0530

    base url changed

--- frontend/src/Services/Base_url.jsx
+++ frontend/src/Services/Base_url.jsx
@@ -1,7 +1,7 @@
 // export const Base_url = "https://marketjourney.in";//main
 
-export const Base_url="http://192.168.29.152:6003"
+// export const Base_url="http://192.168.29.152:6003"
 // export const Base_url="http://192.168.29.77:6003"//localhost:6003
-// export const Base_url="https://seclobtest.site"//test server
+export const Base_url="https://admin.marketjourney.in"//test server
 
 // export const Base_url="http://localhost:6003"

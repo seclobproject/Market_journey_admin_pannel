6927b484bbe0462dbe1a74fd54bd1c8da0c2f0ec
ShyamKumarA <shyamkumarbeypore@gmail.com>
2024-04-24 18:15:40 +0530

    check

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Serving static files
-app.use("/uploads", express.static("/var/www/seclob/marketjourney/adminfolder/uploads"));
-// app.use("/uploads", express.static("uploads"));
+// app.use("/uploads", express.static("/var/www/seclob/marketjourney/adminfolder/uploads"));
+app.use("/uploads", express.static("uploads"));
 
 // Routes
 app.use("/api/user", userRouter);
@@ -36,15 +36,15 @@ app.use((err, req, res, next) => {
 });
 
 // Production setup
-if (NODE_ENV === "production") {
-    // Serve frontend files
-    app.use(express.static("/var/www/seclob/marketjourney/adminfolder/frontend/dist"));
+// if (NODE_ENV === "production") {
+//     // Serve frontend files
+//     app.use(express.static("/var/www/seclob/marketjourney/adminfolder/frontend/dist"));
 
-    // Handle React routing, return all requests to React app
-    app.get("*", (req, res) => {
-     res.sendFile("/var/www/seclob/marketjourney/adminfolder/frontend/dist/index.html");
-    });
-}
+//     // Handle React routing, return all requests to React app
+//     app.get("*", (req, res) => {
+//      res.sendFile("/var/www/seclob/marketjourney/adminfolder/frontend/dist/index.html");
+//     });
+// }
 
 // Starting the server
 const PORT = process.env.PORT || 6003;

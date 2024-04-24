b7ebc0d21de5b0077d1bbf31d39d7a916c14a317
VysakhSdev <vysakhs807@gmail.com>
2024-04-24 19:02:34 +0530

    edits

--- frontend/src/Services/Base_url.jsx
+++ frontend/src/Services/Base_url.jsx
@@ -1,6 +1,6 @@
 
-export const Base_url="http://192.168.29.152:6003"
+// export const Base_url="http://192.168.29.152:6003"
 // export const Base_url="http://192.168.29.77:6003"//localhost:6003
-// export const Base_url="https://admin.marketjourney.in"//test server
+export const Base_url="https://admin.marketjourney.in"//test server
 
 // export const Base_url="http://localhost:6003"
